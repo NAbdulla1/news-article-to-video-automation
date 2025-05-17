3f74f844b8a4167cebfcea1e6cc77dda2e345fe6
Md. Abdulla Al Mamun <nayonabdulla@gmail.com>
2025-05-18 00:16:52 +0600

    Show circular progress bar while processing a news article

--- NewsAppTTSNode/app/src/main/java/com/nabdulla/newsappttsnode/NewsArticleAdapter.java
+++ NewsAppTTSNode/app/src/main/java/com/nabdulla/newsappttsnode/NewsArticleAdapter.java
@@ -3,6 +3,7 @@ package com.nabdulla.newsappttsnode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -35,9 +36,15 @@ public class NewsArticleAdapter extends RecyclerView.Adapter<NewsArticleAdapter.
     public void onBindViewHolder(@NonNull NewsArticleViewHolder holder, int position) {
         TextView tvNewsTitle = holder.itemView.findViewById(R.id.tvNewsTitle);
         TextView tvStatus = holder.itemView.findViewById(R.id.tvStatus);
+        ProgressBar pbProcessing = holder.itemView.findViewById(R.id.pbProcessing);
 
         tvNewsTitle.setText(this.newsArticles.get(position).getArticle().substring(0, 25));
         tvStatus.setText(this.newsArticles.get(position).getStatus().toString());
+        if (this.newsArticles.get(position).getStatus() == NewsArticleStatus.PROCESSING) {
+            pbProcessing.setVisibility(View.VISIBLE);
+        } else {
+            pbProcessing.setVisibility(View.GONE);
+        }
     }
 
     @Override
