cf4c0d0144408a66c5aa5077405205acbcf5c9aa
Md. Abdulla Al Mamun <nayonabdulla@gmail.com>
2025-09-07 08:17:49 +0000

    add unit test and test news-scarappers

---
+++ NewsScrapper/__tests__/prothom-alo/ContentScrapper.test.js
@@ -0,0 +1,63 @@
+import { chromium } from "playwright";
+import { ContentScrapper } from "../../prothom-alo/ContentScrapper";
+import { InterviewContentScrapper } from "../../prothom-alo/InterviewContentScrapper";
+
+describe("Prothom Alo Content Scrapper", () => {
+    let browser;
+    let page;
+    let scrapper;
+
+    beforeAll(async () => {
+        browser = await chromium.launch({ headless: true });
+    });
+
+    beforeEach(async () => {
+        page = await browser.newPage();
+    });
+
+    test("should scrap content from a valid article URL", async () => {
+        scrapper = new ContentScrapper(page);
+        const urlToScrap = "https://www.prothomalo.com/opinion/editorial/fs7rjf5est";
+        const result = await scrapper.scrapContent(urlToScrap);
+
+        expect(result).toHaveProperty("headline");
+        expect(result).toHaveProperty("author");
+        expect(result).toHaveProperty("content");
+
+        expect(typeof result.headline).toBe("string");
+        expect(typeof result.content).toBe("string");
+
+        expect(result.headline.length).toBeGreaterThan(0);
+        expect(result.content.length).toBeGreaterThan(0);
+    }, 5 * 60000);
+
+    test("should handle non-existent article URL gracefully", async () => {
+        scrapper = new ContentScrapper(page);
+        const urlToScrap = "https://www.prothomalo.com/opinion/article/non-existent-article";
+        await expect(scrapper.scrapContent(urlToScrap)).rejects.toThrow();
+    }, 5 * 60000);
+
+    test("should interview article proprly", async () => {
+        scrapper = new InterviewContentScrapper(page);
+        const urlToScrap = "https://www.prothomalo.com/opinion/interview/khludjm83o";
+        const result = await scrapper.scrapContent(urlToScrap);
+
+        expect(result).toHaveProperty("headline");
+        expect(result).toHaveProperty("author");
+        expect(result).toHaveProperty("content");
+
+        expect(typeof result.headline).toBe("string");
+        expect(typeof result.content).toBe("string");
+
+        expect(result.headline.length).toBeGreaterThan(0);
+        expect(result.content.length).toBeGreaterThan(0);
+    }, 5 * 60000);
+
+    afterEach(async () => {
+        await page.close();
+    });
+
+    afterAll(async () => {
+        await browser.close();
+    });
+});
